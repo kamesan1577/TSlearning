34b15b0d3710b18205cdb841a37183c185d76e6c
kamesan1577 <kamesan1577@users.noreply.github.com>
2023-06-25 17:17:44 +0900

    あ

--- react/sample-app/src/SimpleButton.test.tsx
+++ react/sample-app/src/SimpleButton.test.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { SimpleButton } from "./SimpleButton";
  
 test("ボタンをクリックするとON/OFFの表示が切り替わる", async () => {
+  const user = userEvent.setup();
   render(<SimpleButton />);
   const simpleButton = screen.getByRole("button");
-  //FIXME なぜか通らないテスト
   expect(simpleButton).toHaveTextContent("OFF");
+  await user.click(simpleButton);
+  expect(simpleButton).toHaveTextContent("ON");
 });
